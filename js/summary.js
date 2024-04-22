0e1e7aa9523e4e910c752d0146d7b35a3ba2f740
Nicolas Voges <nicolas.voges@hotmail.com>
2024-04-22 18:40:02 +0200

    add documentation

--- js/summary.js
+++ js/summary.js
@@ -23,15 +23,27 @@ function showSummaryValues() {
 }
 
 
+/**
+ * This function outputs the amount of task with status "To do".
+ */
+
 function renderToDoButton() {
     document.getElementById('summary-to-do-amount').innerHTML = getToDosAmount();
 }
 
 
+/**
+ * This function outputs the amount of task with status "Done".
+ */
+
 function renderDoneButton() {
     document.getElementById('summary-done-amount').innerHTML = getDonesAmount();
 }
 
+
+/**
+ * This function renders the mid button.
+ */
 
 function renderPriorityAndDueDateButton() {
     if (tasks.length > 0) {
@@ -45,6 +57,12 @@ function renderPriorityAndDueDateButton() {
     }
 }
 
+
+/**
+ * This function searchs for all tasks with the highest priority.
+ * 
+ * @returns the tasks with highest priority as an array.
+ */
 
 function findTasks() {
     let results = foundPriority('Urgent');
@@ -61,34 +79,79 @@ function findTasks() {
 }
 
 
+/**
+ * This function sets the styles for the priority icon.
+ * 
+ * @param {string} prio 
+ */
+
 function renderPriorityIcon(prio) {
     let icon = document.getElementById('summary-priority-icon');
     document.getElementById('summary-priority-icon-box').style.backgroundColor = `var(--color-${prio.toLocaleLowerCase()})`;
     switch (prio) {
         case 'Urgent':
-            icon.src = './assets/img/summary-priority-up-down.svg';
-            icon.style.transform = 'unset';
-            icon.style.width = '35px';
-            icon.style.height = '26px';
+            setIconStylesToUrgent(icon);
             break;
         case 'Medium':
-            icon.src = './assets/img/prio-medium-white.svg';
-            icon.style.transform = 'unset';
-            icon.style.width = '30px';
-            icon.style.height = '16px';
+            setIconStylesToMedium(icon);
             break;
         case 'Low':
-            icon.src = './assets/img/summary-priority-up-down.svg';
-            icon.style.transform = 'rotateZ(180deg)';
-            icon.style.width = '35px';
-            icon.style.height = '26px';
+            setIconStylesToLow(icon);
             break;
-
         default:
             break;
     }
 }
 
+
+/**
+ * This function set the icon styles to "Urgent".
+ * 
+ * @param {Element} icon 
+ */
+
+function setIconStylesToUrgent(icon) {
+    icon.src = './assets/img/summary-priority-up-down.svg';
+    icon.style.transform = 'unset';
+    icon.style.width = '35px';
+    icon.style.height = '26px';
+}
+
+
+/**
+ * This function set the icon styles to "Medium".
+ * 
+ * @param {Element} icon 
+ */
+
+function setIconStylesToMedium(icon) {
+    icon.src = './assets/img/prio-medium-white.svg';
+    icon.style.transform = 'unset';
+    icon.style.width = '30px';
+    icon.style.height = '16px';
+}
+
+
+/**
+ * This function set the icon styles to "Low".
+ * 
+ * @param {Element} icon 
+ */
+
+function setIconStylesToLow(icon) {
+    icon.src = './assets/img/summary-priority-up-down.svg';
+    icon.style.transform = 'rotateZ(180deg)';
+    icon.style.width = '35px';
+    icon.style.height = '26px';
+}
+
+
+/**
+ * This function searchs in the aray tasks for all tasks with the same priority.
+ * 
+ * @param {string} prio 
+ * @returns the tasks with the same priority as an array.
+ */
 
 function foundPriority(prio) {
     let results = [];
@@ -101,22 +164,40 @@ function foundPriority(prio) {
 }
 
 
+/**
+ *  This function outputs the amount of Tasks with the highest priority.
+ * 
+ * @param {Array} foundTasks 
+ */
+
 function renderPriorityAmount(foundTasks) {
     document.getElementById('summary-priority-amount').innerHTML = foundTasks.length;
 }
 
+
+/**
+ * This function outputs the value of the highest priority.
+ * 
+ * @param {Array} foundTasks 
+ */
 
 function renderPriorityValue(foundTasks) {
     document.getElementById('summary-priority-value').innerHTML = foundTasks[0].priority;
 }
 
 
+/**
+ * This function searchs for the upcoming due date and outputs it.
+ * 
+ * @param {Array} foundTasks 
+ */
+
 function rednerUpcomingDeadline(foundTasks) {
     let date = Infinity;
     let taskIndex = 0;
     for (let i = 0; i < foundTasks.length; i++) {
         let taskDate = new Date(foundTasks[i].dueDate);
-        if (taskDate < date){
+        if (taskDate < date) {
             date = taskDate;
             taskIndex = i;
         }
@@ -125,15 +206,27 @@ function rednerUpcomingDeadline(foundTasks) {
 }
 
 
+/**
+ * This function outputs the amount of all tasks on Board.
+ */
+
 function renderTaskButton() {
     document.getElementById('summary-tasks-amount').innerHTML = tasks.length;
 }
 
 
+/**
+ * This function outputs the amount of tasks with status "In progress".
+ */
+
 function renderProgressButton() {
     document.getElementById('summary-in-progress-amount').innerHTML = getInProgressAmount();
 }
 
+
+/**
+ * This function outputs the amount of tasks with status "Await feedback".
+ */
 
 function renderFeedbackButton() {
     document.getElementById('summary-await-feedback-amount').innerHTML = getAwaitFeedbackAmount();
