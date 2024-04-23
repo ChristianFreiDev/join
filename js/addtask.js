e51ea538c090cf5d225f9b6e11a114addd3de25a
TimoRenken <timorenken@outlook.de>
2024-04-23 10:00:46 +0200

    need help to switch color of SVG while hover!

---
+++ js/addtask.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     setTimeout(function() {
+//         let clearX = document.getElementById('clear-x');
+
+//         clearX.addEventListener('mouseover', function() {
+//             clearX.src = "/assets/img/clear-x-blue.svg";
+//         });
+
+//         clearX.addEventListener('mouseout', function() {
+//             clearX.src = "/assets/img/clear-x.svg";
+//         });
+//     }, 1000); // Warten Sie 1 Sekunde, bevor Sie den Event-Listener hinzufügen
+// });
+
+// nur zur Demonstration, was ich schon probiert habe.
