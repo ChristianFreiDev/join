34243c4281fb88c31fc8f5f9b8483b8a47694be3
TimoRenken <timorenken@outlook.de>
2024-04-24 10:41:37 +0200

    fixed Log-in Logo animation

--- js/addtask.js
+++ js/addtask.js
@@ -4,14 +4,31 @@
  */
 function clickUrgentButton(){
 let button = document.getElementById('prio-urgent');
-let arrow = document.getElementById('prio-arrow-up');
+let image = document.getElementById('prio-arrow-up');
 
 button.classList.toggle('urgent-button-clicked');
 
-if(arrow.src.match('/assets/img/prio-up.svg')){
-    arrow.src = '/assets/img/prio-up-white.svg';
+if(image.src.match('/assets/img/prio-up.svg')){
+    image.src = '/assets/img/prio-up-white.svg';
 } else {
-    arrow.src = '/assets/img/prio-up.svg'
+    image.src = '/assets/img/prio-up.svg'
 }
 
 }
+
+/** 
+ * This function is used to switch the color of the prio-button "urgent"
+ */
+function clickMediumButton(){
+    let button = document.getElementById('prio-medium');
+    let image = document.getElementById('prio-medium-equals');
+    
+    button.classList.toggle('medium-button-clicked');
+    
+    if(image.src.match('/assets/img/prio-up.svg')){
+        image.src = '/assets/img/prio-up-white.svg';
+    } else {
+        image.src = '/assets/img/prio-up.svg'
+    }
+    
+    }
