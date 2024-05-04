2b3a4c6267276c7b9820598b46c1667bf46750bb
Nicolas Voges <nicolas.voges@hotmail.com>
2024-05-04 20:29:45 +0200

    add documentation to storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,12 @@
 const BASE_URL = "https://testprojekt-2599f-default-rtdb.europe-west1.firebasedatabase.app/";
 
-
+/**
+ * This function stores an object to the firebase storage.
+ * 
+ * @param {string} path 
+ * @param {object} data 
+ * @returns respons as Json.
+ */
 async function setItem(path = "", data = {}) {
     let response = await fetch(BASE_URL + path + '.json', {
         method: "PUT",
@@ -13,13 +19,21 @@ async function setItem(path = "", data = {}) {
 }
 
 
+/**
+ * This function loads data from the firebase storage.
+ * 
+ * @param {string} path 
+ * @returns loaded data as Json.
+ */
 async function getItem(path = "") {
     let response = await fetch(BASE_URL + path + '.json');
     let responseToJSON = await response.json();
     return responseToJSON;
 }
 
-
+/**
+ * This function loads the users- array.
+ */
 async function loadUsers() {
     try {
         users = await getItem("/users")
@@ -29,6 +43,9 @@ async function loadUsers() {
 }
 
 
+/**
+ * This function stores the users- array.
+ */
 async function storeUsers() {
     try {
         console.log('storing users');
@@ -39,6 +56,9 @@ async function storeUsers() {
 }
 
 
+/**
+ * This function loads the tasks- array.
+ */
 async function loadTasks() {
     try {
         tasks = await getItem('/tasks');
@@ -48,6 +68,9 @@ async function loadTasks() {
 }
 
 
+/**
+ * This function stores the tasks- array.
+ */
 async function storeTasks() {
     try {
         console.log('storing tasks');
@@ -58,6 +81,9 @@ async function storeTasks() {
 }
 
 
+/**
+ * This function loads the contacts- array.
+ */
 async function loadContacts() {
     try {
         contacts = await getItem('/contacts');
@@ -67,6 +93,9 @@ async function loadContacts() {
 }
 
 
+/**
+ * This function stores the contacts- array.
+ */
 async function storeContacts() {
     try {
         console.log('storing contacts');
@@ -77,6 +106,9 @@ async function storeContacts() {
 }
 
 
+/**
+ * This function resets the remote Storage.
+ */
 async function resetUsersTasksContacts() {
     try {
         await setItem('/users', offlineUsers);
@@ -88,6 +120,9 @@ async function resetUsersTasksContacts() {
 }
 
 
+/**
+ * This function resets the tasks-, users-, and contacts- array.
+ */
 async function useOfflineData() {
     tasks = offlineTasks;
     users = offlineUsers;
@@ -95,21 +130,45 @@ async function useOfflineData() {
 }
 
 
+/**
+ * This function saves a variable to the local storage.
+ * 
+ * @param {string} key 
+ * @param {number, string or boolean} variable 
+ */
 function saveVariableInLocalStorage(key, variable) {
     localStorage.setItem(key, variable)
 }
 
 
+/**
+ * This function loads a variable from the local storage.
+ * 
+ * @param {string} key 
+ * @returns 
+ */
 function loadVariableFromLocalStorage(key) {
     return localStorage.getItem(key)
 }
 
 
+/**
+ * This function saves an array to the local storage.
+ * 
+ * @param {string} key 
+ * @param {array} array 
+ */
 function saveArrayInLocalStorage(key, array) {
     localStorage.setItem(key, JSON.stringify(array));
 }
 
 
+/**
+ * This function loads an array fron the local storage.
+ * 
+ * @param {string} key 
+ * @returns 
+ */
 function loadArrayFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key));
 }
