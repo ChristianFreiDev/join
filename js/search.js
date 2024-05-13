b5b6ac7106e7a7d6aa8ff42f9b35242418923f11
Christian Frei <christian.frei.01@googlemail.com>
2024-05-14 00:58:27 +0200

    refactor search.js

--- js/search.js
+++ js/search.js
@@ -17,6 +17,35 @@ function searchTasks() {
 
 
 /**
+ * This function filters the user names using a search string.
+ * @returns {Array} users whose names match the search string
+ */
+function filterUserNames() {
+    return users.filter(user => {
+        let fullUserName = `${user.firstName.toLowerCase()} ${user.lastName.toLowerCase()}`;
+        return fullUserName.includes(searchString);
+    });
+}
+
+
+/**
+ * This function displays the users that were found as a list of options.
+ * @param {Object} task 
+ * @param {Array} foundUsers 
+ * @param {string} idPrefix prefix for selecting the correct element ids, either 'add-task' or 'edit-task' 
+ */
+function showFoundUsers(task, foundUsers, idPrefix) {
+    let taskAssignedTo = document.getElementById(`${idPrefix}-assigned-to`);
+    if (foundUsers.length > 0) {
+        taskAssignedTo.innerHTML = renderSelectOptions(task, foundUsers, idPrefix);
+    } else {
+        taskAssignedTo.innerHTML = '<div class="no-users-message">No users found</div>';
+    }
+    taskAssignedTo.classList.remove('display-none');
+}
+
+
+/**
  * This function searches for users matching the search criteria.
  * @param {number} taskId 
  * @param {string} idPrefix prefix for selecting the correct element ids, either 'add-task' or 'edit-task' 
@@ -30,15 +59,6 @@ function searchUsers(taskId, idPrefix) {
     }
     let searchInput = document.getElementById(`${idPrefix}-drop-down-input`);
     let searchString = searchInput.value.toLowerCase();
-    foundUsers = users.filter(user => {
-        let fullUserName = `${user.firstName.toLowerCase()} ${user.lastName.toLowerCase()}`;
-        return fullUserName.includes(searchString);
-    });
-    let taskAssignedTo = document.getElementById(`${idPrefix}-assigned-to`);
-    if (foundUsers.length > 0) {
-        taskAssignedTo.innerHTML = renderSelectOptions(task, foundUsers, idPrefix);
-    } else {
-        taskAssignedTo.innerHTML = '<div class="no-users-message">No users found</div>';
-    }
-    taskAssignedTo.classList.remove('display-none');
+    foundUsers = filterUserNames(searchString);
+    showFoundUsers();
 }
